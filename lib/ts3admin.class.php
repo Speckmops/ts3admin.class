d401a9ad8e490e02d9675cb661b51bd5ab7b8235
Stefano Kocka <ares308@gmail.com>
2017-05-12 19:43:36 +0200

    Minor tweaks
    - less memory
    - less cpu cycles
    - less code
    - better code readability

--- lib/ts3admin.class.php
+++ lib/ts3admin.class.php
@@ -4844,61 +4844,31 @@ class ts3admin {
 				return $this->generateOutput(true, array(), true);
 			}
 			
-			if($mode == 'array') {
-				if(empty($fetchData['data'])) { return $this->generateOutput(true, array(), array()); }
-				$datasets = explode(' ', $fetchData['data']);
-				
-				$output = array();
-				
-				foreach($datasets as $dataset) {
-					$dataset = explode('=', $dataset);
-					
-					if(count($dataset) > 2) {
-						for($i = 2; $i < count($dataset); $i++) {
-							$dataset[1] .= '='.$dataset[$i];
-						}
-						$output[$this->unEscapeText($dataset[0])] = $this->unEscapeText($dataset[1]);
-					}else{
-						if(count($dataset) == 1) {
-							$output[$this->unEscapeText($dataset[0])] = '';
-						}else{
-							$output[$this->unEscapeText($dataset[0])] = $this->unEscapeText($dataset[1]);
-						}
-						
-					}
-				}
-				return $this->generateOutput(true, array(), $output);
-			}
-			if($mode == 'multi') {
+			if($mode == 'array' || $mode == 'multi'){
 				if(empty($fetchData['data'])) { return $this->generateOutput(true, array(), array()); }
 				$datasets = explode('|', $fetchData['data']);
-				
+
 				$output = array();
-				
+
 				foreach($datasets as $datablock) {
 					$datablock = explode(' ', $datablock);
-					
+
 					$tmpArray = array();
-					
+
 					foreach($datablock as $dataset) {
-						$dataset = explode('=', $dataset);
-						if(count($dataset) > 2) {
-							for($i = 2; $i < count($dataset); $i++) {
-								$dataset[1] .= '='.$dataset[$i];
-							}
-							$tmpArray[$this->unEscapeText($dataset[0])] = $this->unEscapeText($dataset[1]);
-						}else{
-							if(count($dataset) == 1) {
-								$tmpArray[$this->unEscapeText($dataset[0])] = '';
-							}else{
-								$tmpArray[$this->unEscapeText($dataset[0])] = $this->unEscapeText($dataset[1]);
-							}
-						}					
+						$dataset = explode('=', $dataset, 2);
+						$tmpArray[ $dataset[ 0 ] ] = isset( $dataset[ 1 ] ) ? $this->unEscapeText( $dataset[ 1 ] ) : '';				
 					}
+					
+					if($mode == 'array'){
+						return $this->generateOutput(true, array(), $tmpArray);
+					}
+
 					$output[] = $tmpArray;
 				}
 				return $this->generateOutput(true, array(), $output);
 			}
+			
 			if($mode == 'plain') {
 				return $fetchData;
 			}
